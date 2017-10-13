10a281190e61125115c6c4e41c63ca305f1a9ccd
Moucadel Julien <julien.moucadel@gmail.com>
2017-10-13 03:20:45 +0200

    clone 42

---
+++ d09/ex00/script.js
@@ -0,0 +1,7 @@
+function	gonfler()
+{
+	var 	ballon = document.getElementById('ballon');
+
+	ballon.style.width = ballon.offsetWidth + 20 + "px"
+	ballon.style.height = ballon.offsetHeight + 20 + "px"
+}
